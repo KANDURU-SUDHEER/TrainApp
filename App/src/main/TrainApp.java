9aa251f2cd08f10adbce26597e8c39277c5e9196
KANDURU-SUDHEER <sk5364@srmist.edu.in>
2026-04-22 10:58:31 +0530

    KANDURU-SUDHEER Added UC11-Validate Train ID & Cargo Codes

--- App/src/main/TrainApp.java
+++ App/src/main/TrainApp.java
@@ -1,13 +1,11 @@
-package main; /**
+package main;
+
+/**
  * ================================================================
  * MAIN CLASS - TrainApp
  * ================================================================
  *
  * Use Case 10: Count Total Seats in Train (reduce)
- *
- * Description:
- * This class aggregates seating capacity of all bogies
- * into a single total using Stream reduce().
  *
  * Author: KANDURU-SUDHEER
  * Version: 10.0
@@ -17,7 +15,6 @@ import java.util.*;
 
 public class TrainApp {
 
-    // Bogie class
     static class Bogie {
         String name;
         int capacity;
@@ -39,7 +36,6 @@ public class TrainApp {
         System.out.println("   UC10 - Count Total Seats in Train    ");
         System.out.println("========================================\n");
 
-        // Create list of bogies
         List<Bogie> bogies = new ArrayList<>();
 
         bogies.add(new Bogie("Sleeper", 72));
@@ -47,22 +43,15 @@ public class TrainApp {
         bogies.add(new Bogie("First Class", 24));
         bogies.add(new Bogie("Sleeper", 70));
 
-        // Display bogies
         System.out.println("Bogies in Train:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
 
-        // ============================================================
-        // AGGREGATION USING STREAM (map + reduce)
-        // ============================================================
         int totalSeats = bogies.stream()
-                .map(b -> b.capacity)         // extract capacity
-                .reduce(0, Integer::sum);    // sum all
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        // Display total seats
         System.out.println("\nTotal Seating Capacity: " + totalSeats);
-
-        System.out.println("\nUC10 aggregation completed...");
     }
 }
